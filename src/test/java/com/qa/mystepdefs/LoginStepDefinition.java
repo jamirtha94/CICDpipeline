610f0a32a17ce7f6cb6a19b560d32cb5c7cddd39
Thanga Amirtha <amirtha@ibm.com>
2025-10-11 16:58:22 -0300

    CICD test

--- src/test/java/com/qa/mystepdefs/LoginStepDefinition.java
+++ src/test/java/com/qa/mystepdefs/LoginStepDefinition.java
@@ -1,45 +1,44 @@
 package com.qa.mystepdefs;
+import io.cucumber.java.*;
 import io.cucumber.java.bs.A;
 import io.cucumber.java.en.*;
-import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import pages.LoginPage;
 
 
 import static org.junit.Assert.assertTrue;
 
 public class LoginStepDefinition{
-    WebDriver driver = new ChromeDriver();
+    private LoginPage loginPage;
+    private WebDriver driver =null;
+    @Before
+    public void setup() {
+        driver = new ChromeDriver();
+    }
+    @After
+    public void tearDown(){
+        if(driver!= null){
+            driver.quit();
+        }
+    }
 
     @Given ("the user is on the login page")
     public void login_page(){
         System.out.println("login");
         driver.navigate().to("https://www.saucedemo.com/v1/");
-        String title = driver.getTitle();
-        String expectedtitle = "Swag1 Labs";
-        try{
-        
-            System.out.println("Assertion Passed");
-            driver.findElement(By.id("user-name")).sendKeys("standard_user");
-            driver.findElement(By.id("password")).sendKeys("secret_sauce");
-            driver.findElement(By.id("login-button")).click();
-            System.out.println(driver.getTitle());
-
-        }
-     catch (AssertionError ae){
-         System.out.println(ae.getMessage());
-     }
-        finally{
-            driver.quit();
-        }
-
+        loginPage = new LoginPage(driver);
 
     }
     @When ("the user enters valid credentials")
 
     public void credentials(){
+        loginPage.enterEmail("standard_user");
+        loginPage.enterPassword("secret_sauce");
+        loginPage.clickLoginButton();
         System.out.println("credentials");
     }
 
@@ -48,14 +47,21 @@ public class LoginStepDefinition{
         System.out.println("credentials");
     }
 
-    @When ("the user enters invalid credentials")
-    public void invalid_cred(){
+    @When("the user enters invalid {string} and {string}")
+    public void invalid_cred(String username, String password) {
+        loginPage.login(username, password);
+        String actual = driver.getTitle();
+        String expectedTitle = "Swag Labs";
+        Assert.assertEquals(expectedTitle, actual);
+        System.out.println("Assertion Passed");
         System.out.println("invalid credentials");
     }
 
-    @Then ("an error message should be displayed")
-    public void error(){
-        System.out.println("error");
+    @Then ("an {string} message should be displayed")
+    public void error(String error){
+        WebElement actual_error = driver.findElement(By.xpath("//*[@id=\"login_button_container\"]/div/form/h3"));
+        String actualerror = actual_error.getText();
+        System.out.println("error message is:"+ actualerror);
     }
 
 }
